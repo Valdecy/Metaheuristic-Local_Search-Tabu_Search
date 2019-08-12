0bbc84fffdba11f2633478f0454418f0ce84011f
Valdecy Pereira <valdecypereira@yahoo.com.br>
2019-08-12 09:33:50 -0300

    Update Python-MH-Local Search-Tabu Search.py

--- Python-MH-Local Search-Tabu Search.py
+++ Python-MH-Local Search-Tabu Search.py
@@ -23,14 +23,14 @@ def distance_calc(Xdata, city_tour):
     distance = 0
     for k in range(0, len(city_tour[0])-1):
         m = k + 1
-        distance = distance + Xdata.iloc[city_tour[0][k]-1, city_tour[0][m]-1]            
+        distance = distance + Xdata[city_tour[0][k]-1, city_tour[0][m]-1]            
     return distance
 
 # Function: Euclidean Distance 
 def euclidean_distance(x, y):       
     distance = 0
     for j in range(0, len(x)):
-        distance = (x.iloc[j] - y.iloc[j])**2 + distance   
+        distance = (x[j] - y[j])**2 + distance   
     return distance**(1/2) 
 
 # Function: Initial Seed
@@ -44,66 +44,64 @@ def seed_function(Xdata):
 
 # Function: Build Distance Matrix
 def buid_distance_matrix(coordinates):
-    Xdata = pd.DataFrame(np.zeros((coordinates.shape[0], coordinates.shape[0])))
+    Xdata = np.zeros((coordinates.shape[0], coordinates.shape[0]))
     for i in range(0, Xdata.shape[0]):
         for j in range(0, Xdata.shape[1]):
             if (i != j):
-                x = coordinates.iloc[i,:]
-                y = coordinates.iloc[j,:]
-                Xdata.iloc[i,j] = euclidean_distance(x, y)        
+                x = coordinates[i,:]
+                y = coordinates[j,:]
+                Xdata[i,j] = euclidean_distance(x, y)        
     return Xdata
 
 # Function: Tour Plot
 def plot_tour_distance_matrix (Xdata, city_tour):
-    m = Xdata.copy(deep = True)
+    m = np.copy(Xdata)
     for i in range(0, Xdata.shape[0]):
         for j in range(0, Xdata.shape[1]):
-            m.iloc[i,j] = (1/2)*(Xdata.iloc[0,j]**2 + Xdata.iloc[i,0]**2 - Xdata.iloc[i,j]**2)    
-    m = m.values
+            m[i,j] = (1/2)*(Xdata[0,j]**2 + Xdata[i,0]**2 - Xdata[i,j]**2)    
     w, u = np.linalg.eig(np.matmul(m.T, m))
     s = (np.diag(np.sort(w)[::-1]))**(1/2) 
     coordinates = np.matmul(u, s**(1/2))
     coordinates = coordinates.real[:,0:2]
-    xy = pd.DataFrame(np.zeros((len(city_tour[0]), 2)))
+    xy = np.zeros((len(city_tour[0]), 2))
     for i in range(0, len(city_tour[0])):
         if (i < len(city_tour[0])):
-            xy.iloc[i, 0] = coordinates[city_tour[0][i]-1, 0]
-            xy.iloc[i, 1] = coordinates[city_tour[0][i]-1, 1]
+            xy[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][i]-1, 1]
         else:
-            xy.iloc[i, 0] = coordinates[city_tour[0][0]-1, 0]
-            xy.iloc[i, 1] = coordinates[city_tour[0][0]-1, 1]
-    plt.plot(xy.iloc[:,0], xy.iloc[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
-    plt.plot(xy.iloc[0,0], xy.iloc[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
-    plt.plot(xy.iloc[1,0], xy.iloc[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+            xy[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy[:,0], xy[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy[0,0], xy[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy[1,0], xy[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
     return
 
 # Function: Tour Plot
 def plot_tour_coordinates (coordinates, city_tour):
-    coordinates = coordinates.values
-    xy = pd.DataFrame(np.zeros((len(city_tour[0]), 2)))
+    xy = np.zeros((len(city_tour[0]), 2))
     for i in range(0, len(city_tour[0])):
         if (i < len(city_tour[0])):
-            xy.iloc[i, 0] = coordinates[city_tour[0][i]-1, 0]
-            xy.iloc[i, 1] = coordinates[city_tour[0][i]-1, 1]
+            xy[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][i]-1, 1]
         else:
-            xy.iloc[i, 0] = coordinates[city_tour[0][0]-1, 0]
-            xy.iloc[i, 1] = coordinates[city_tour[0][0]-1, 1]
-    plt.plot(xy.iloc[:,0], xy.iloc[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
-    plt.plot(xy.iloc[0,0], xy.iloc[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
-    plt.plot(xy.iloc[1,0], xy.iloc[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+            xy[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy[:,0], xy[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy[0,0], xy[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy[1,0], xy[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
     return
 
 # Function:  Build Recency Based Memory and Frequency Based Memory (STM and LTM)
 def build_stm_and_ltm(Xdata):
     n = int((Xdata.shape[0]**2 - Xdata.shape[0])/2)
-    stm_and_ltm = pd.DataFrame(np.zeros((n, 5)), columns = ['City 1','City 2','Recency', 'Frequency', 'Distance'])
+    stm_and_ltm = np.zeros((n, 5)) # ['City 1','City 2','Recency', 'Frequency', 'Distance']
     count = 0
     for i in range (0, int((Xdata.shape[0]**2))):
         city_1 = i // (Xdata.shape[1])
         city_2 = i %  (Xdata.shape[1])
         if (city_1 < city_2):
-            stm_and_ltm.iloc[count, 0] = city_1 + 1
-            stm_and_ltm.iloc[count, 1] = city_2 + 1
+            stm_and_ltm[count, 0] = city_1 + 1
+            stm_and_ltm[count, 1] = city_2 + 1
             count = count + 1
     return stm_and_ltm
 
@@ -135,14 +133,15 @@ def local_search_2_opt(Xdata, city_tour):
 
 # Function: Diversification
 def ltm_diversification (Xdata, stm_and_ltm, city_list):
-    stm_and_ltm = stm_and_ltm.sort_values(['Frequency', 'Distance'], ascending = [True, True])
+    stm_and_ltm = stm_and_ltm[stm_and_ltm[:,3].argsort()]
+    stm_and_ltm = stm_and_ltm[stm_and_ltm[:,4].argsort()]
     lenght = random.sample((range(1, int(Xdata.shape[0]/3))), 1)[0]
     for i in range(0, lenght):
-        m = int(stm_and_ltm.iloc[i, 0] - 1)
-        n = int(stm_and_ltm.iloc[i, 1] - 1)
+        m = int(stm_and_ltm[i, 0] - 1)
+        n = int(stm_and_ltm[i, 1] - 1)
         city_list = local_search_2_swap(Xdata, city_list, m, n)
-        stm_and_ltm.iloc[i, 3] = stm_and_ltm.iloc[i, 3] + 1
-        stm_and_ltm.iloc[i, 2] = 1
+        stm_and_ltm[i, 3] = stm_and_ltm[i, 3] + 1
+        stm_and_ltm[i, 2] = 1
     return stm_and_ltm, city_list
 
 # Function: 4 opt Stochastic
@@ -173,31 +172,31 @@ def local_search_4_opt_stochastic(Xdata, city_tour):
         best_route = copy.deepcopy(best_route_22)            
     elif(best_route_27[1]  < best_route[1]):
         best_route = copy.deepcopy(best_route_27)          
-    return best_route	
-
+    return best_route
+	
 # Function: Tabu Update
 def tabu_update(Xdata, stm_and_ltm, city_list, best_distance, tabu_list, tabu_tenure = 20, diversify = False):
     m_list = []
     n_list = []
     city_list = local_search_2_opt(Xdata, city_list) # itensification
     for i in range(0, stm_and_ltm.shape[0]):
-        m = int(stm_and_ltm.iloc[i, 0] - 1)
-        n = int(stm_and_ltm.iloc[i, 1] - 1)
-        stm_and_ltm.iloc[i, -1] = local_search_2_swap(Xdata, city_list, m, n)[1] 
-    stm_and_ltm = stm_and_ltm.sort_values(by = 'Distance')
-    m = int(stm_and_ltm.iloc[0,0]-1)
-    n = int(stm_and_ltm.iloc[0,1]-1)
-    recency = int(stm_and_ltm.iloc[0,2])
-    distance = stm_and_ltm.iloc[0,-1]     
+        m = int(stm_and_ltm[i, 0] - 1)
+        n = int(stm_and_ltm[i, 1] - 1)
+        stm_and_ltm[i, -1] = local_search_2_swap(Xdata, city_list, m, n)[1] 
+    stm_and_ltm = stm_and_ltm[stm_and_ltm[:,4].argsort()] # Distance
+    m = int(stm_and_ltm[0,0]-1)
+    n = int(stm_and_ltm[0,1]-1)
+    recency = int(stm_and_ltm[0,2])
+    distance = stm_and_ltm[0,-1]     
     if (distance < best_distance): # Aspiration Criterion -> by Objective
         city_list = local_search_2_swap(Xdata, city_list, m, n)
         i = 0
         while (i < stm_and_ltm.shape[0]):
-            if (stm_and_ltm.iloc[i, 0] == m + 1 and stm_and_ltm.iloc[i, 1] == n + 1):
-                stm_and_ltm.iloc[i, 2] = 1
-                stm_and_ltm.iloc[i, 3] = stm_and_ltm.iloc[i, 3] + 1
-                stm_and_ltm.iloc[i, -1] = distance
-                if (stm_and_ltm.iloc[i, 3] == 1):
+            if (stm_and_ltm[i, 0] == m + 1 and stm_and_ltm[i, 1] == n + 1):
+                stm_and_ltm[i, 2] = 1
+                stm_and_ltm[i, 3] = stm_and_ltm[i, 3] + 1
+                stm_and_ltm[i, -1] = distance
+                if (stm_and_ltm[i, 3] == 1):
                     m_list.append(m + 1)
                     n_list.append(n + 1)
                 i = stm_and_ltm.shape[0]
@@ -205,18 +204,18 @@ def tabu_update(Xdata, stm_and_ltm, city_list, best_distance, tabu_list, tabu_te
     else:
         i = 0
         while (i < stm_and_ltm.shape[0]):
-            m = int(stm_and_ltm.iloc[i,0]-1)
-            n = int(stm_and_ltm.iloc[i,1]-1)
-            recency = int(stm_and_ltm.iloc[i,2]) 
+            m = int(stm_and_ltm[i,0]-1)
+            n = int(stm_and_ltm[i,1]-1)
+            recency = int(stm_and_ltm[i,2]) 
             distance = local_search_2_swap(Xdata, city_list, m, n)[1]
             if (distance < best_distance):
                 city_list = local_search_2_swap(Xdata, city_list, m, n)
             if (recency == 0):
                 city_list = local_search_2_swap(Xdata, city_list, m, n)
-                stm_and_ltm.iloc[i, 2] = 1
-                stm_and_ltm.iloc[i, 3] = stm_and_ltm.iloc[i, 3] + 1
-                stm_and_ltm.iloc[i, -1] = distance
-                if (stm_and_ltm.iloc[i, 3] == 1):
+                stm_and_ltm[i, 2] = 1
+                stm_and_ltm[i, 3] = stm_and_ltm[i, 3] + 1
+                stm_and_ltm[i, -1] = distance
+                if (stm_and_ltm[i, 3] == 1):
                     m_list.append(m + 1)
                     n_list.append(n + 1)
                 i = stm_and_ltm.shape[0]
@@ -227,10 +226,10 @@ def tabu_update(Xdata, stm_and_ltm, city_list, best_distance, tabu_list, tabu_te
     if (len(tabu_list[0]) > tabu_tenure):
         i = 0
         while (i < stm_and_ltm.shape[0]):
-            if (stm_and_ltm.iloc[i, 0] == tabu_list[0][0] and stm_and_ltm.iloc[i, 1] == tabu_list[1][0]):
+            if (stm_and_ltm[i, 0] == tabu_list[0][0] and stm_and_ltm[i, 1] == tabu_list[1][0]):
                 del tabu_list[0][0]
                 del tabu_list[1][0]
-                stm_and_ltm.iloc[i, 2] = 0
+                stm_and_ltm[i, 2] = 0
                 i = stm_and_ltm.shape[0]
             i = i + 1          
     if (diversify == True):
@@ -266,13 +265,33 @@ def tabu_search(Xdata, city_tour, iterations = 150, tabu_tenure = 20):
 
 ######################## Part 1 - Usage ####################################
 
-X = pd.read_csv('Python-MH-Local Search-Tabu Search-Dataset-01.txt', sep = '\t') # 17 cities = 1922.33
-seed = seed_function(X)
-lsts = tabu_search(X, city_tour = seed, iterations = 50, tabu_tenure = 7)
-plot_tour_distance_matrix(X, lsts) # Red Point = Initial city; Orange Point = Second City # The generated coordinates (2D projection) are aproximated, depending on the data, the optimum tour may present crosses.
+# Load File - A Distance Matrix (17 cities,  optimal = 1922.33)
+X = pd.read_csv('Python-MH-Local Search-Tabu Search-Dataset-01.txt', sep = '\t') 
+X = X.values
 
-Y = pd.read_csv('Python-MH-Local Search-Tabu Search-Dataset-02.txt', sep = '\t') # Berlin 52 = 7544.37
-X = buid_distance_matrix(Y)
+# Start a Random Seed
 seed = seed_function(X)
-lsts = tabu_search(X, city_tour = seed, iterations = 100, tabu_tenure = 7)
-plot_tour_coordinates (Y, lsts) # Red Point = Initial city; Orange Point = Second City
+
+# Call the Function
+lsts = tabu_search(X, city_tour = seed, iterations = 50, tabu_tenure = 7)
+
+# Plot Solution. Red Point = Initial city; Orange Point = Second City # The generated coordinates (2D projection) are aproximated, depending on the data, the optimum tour may present crosses
+plot_tour_distance_matrix(X, lsts)
+
+######################## Part 2 - Usage ####################################
+
+# Load File - Coordinates (Berlin 52,  optimal = 7544.37)
+Y = pd.read_csv('Python-MH-Local Search-Tabu Search-Dataset-02.txt', sep = '\t') 
+Y = Y.values
+
+# Build the Distance Matrix
+X = buid_distance_matrix(Y)
+
+# Start a Random Seed
+seed = seed_function(X)
+
+# Call the Function
+lsts = tabu_search(X, city_tour = seed, iterations = 200, tabu_tenure = 20)
+
+# Plot Solution. Red Point = Initial city; Orange Point = Second City
+plot_tour_coordinates(Y, lsts)
